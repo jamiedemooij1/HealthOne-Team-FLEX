7d6b7e942afb7596eb4f60082f3cea4ea1927ab6
Thierry Broersma <302677794@student.rocmondriaan.nl>
2021-10-04 10:00:12 +0200

    Pagina formgeveing aangepast met columns zodat de pagina in breedte verdeelt is in info onver Healthone en het nieuwsoverzicht

--- Templates/home.php
+++ Templates/home.php
@@ -8,24 +8,37 @@
     ?>
     <body>
         <div class="container-fluid">
+
+                <?php
+                include_once ('defaults/header.php');
+                ?>
+
             <?php
-            include_once ('defaults/header.php');
             include_once ('defaults/menu.php');
             include_once ('defaults/pictures.php');
             ?>
-            <main>
-                <h4>Sportcenter HealthOne</h4>
-                Fit en gezond zijn is geen vanzelfsprekendheid. We moeten er zelf wat voor doen. Goede, gezonde voeding is hiervoor de basis.
-                Bewegen hoort hier ook bij. Regelmatig bewegen zorgt voor een goede doorbloeding en draagt bij aan ontspanning van lichaam en geest.
-                Sporten is goed voor sterkere spieren en voor de conditie. Sporcenter HealthOne heeft verschillende sportapparaten om mee te kunnen werken aan je conditie.
-                <hr>
-            </main>
-            <aside>
+                <div class="row">
+                    <div class="flex-column">
+                        <h4>Sportcenter HealthOne</h4>
+                        Fit en gezond zijn is geen vanzelfsprekendheid. We moeten er zelf wat voor doen. Goede, gezonde voeding is hiervoor de basis.
+                        Bewegen hoort hier ook bij. Regelmatig bewegen zorgt voor een goede doorbloeding en draagt bij aan ontspanning van lichaam en geest.
+                        Sporten is goed voor sterkere spieren en voor de conditie. Sporcenter HealthOne heeft verschillende sportapparaten om mee te kunnen werken aan je conditie.
+                        <hr>
+                    </div>
+                    <div class="flex-column">
+                        <h4>Nieuwsoverzicht</h4>
+                    </div>
+                </div>
 
-            </aside>
-            <?php
-            include_once ('defaults/footer.php');
-            ?>
+
+
+
+
+
+                <?php
+                include_once ('defaults/footer.php');
+                ?>
+
         </div>
     </body>
 </html>
