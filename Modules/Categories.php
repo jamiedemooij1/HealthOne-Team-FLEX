f92212cd6237a9bc853d01f864bf5d5be5b8f7b6
Thierry Broersma <302677794@student.rocmondriaan.nl>
2021-10-05 13:45:25 +0200

    categorie pagina aangepast door het met een query zich weer te geven

--- Modules/Categories.php
+++ Modules/Categories.php
@@ -1,8 +1,17 @@
 <?php
+$host = '127.0.0.1';
+$db   = 'healthone';
+$user = 'root';
+$pass = '';
+$charset = 'utf8mb4';
 // TODO Zorg dat de methodes goed ingevuld worden met de juiste queries.
 function getCategories(/*int $id, varchar $name, varchar $picture, varchar $description*/)
 {
-
+        $db = new PDO("mysql:host=localhost;dbname=healthone", "root", "");
+        $query = $db->prepare("SELECT * FROM product");
+        $query->execute();
+        $result = $query->fetchAll(PDO::FETCH_CLASS);
+        return $result;
 }
 
 function getCategoryName(int $id)
