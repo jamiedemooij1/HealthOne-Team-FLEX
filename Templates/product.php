7cac8b4476a511fcb37bc74e1207d43f2bbbb41c
Thierry Broersma <302677794@student.rocmondriaan.nl>
2021-10-21 10:09:23 +0200

    Bezig met de product pagina te stylen naar ons functioneel ontwerp

--- Templates/product.php
+++ Templates/product.php
@@ -21,13 +21,24 @@ include_once('defaults/head.php');
         <?php
         global $getproducts;
         global $product;
-        var_dump($product);
         foreach ($getproducts as &$data) {
-            echo "<h4>" . $data->Name .  "</h4>
-                  <p> " . $data->Description . "</p>
-                  <a href=". 3 .  "/products>
-                    <img src='" . $data->Picture . "' class = 'product'>
-                  </a>
+            echo "
+            <div class='flex-column'>
+                <h4>" . $data->Name .  "</h4>
+                <p class='description'> " . $data->Description . "</p>
+                <h4 class='article-head'>Mijn reviews</h4>
+                <article class='article-one'>
+                    <h5><b>Mijn rating is 4 sterren</b></h5>
+                    <p>Ik was de 1e keer begonnen op het roeiapparaat Tunturi FitRow 70 Water. Het was geweldig om binnen 15 minuten 100 calorieën verbrand
+                        te hebbben en al mijn spieren goed kunnen gebruiken. Een hele goeie ervaring, voelde me echt een topsporter!
+                    </p>
+                </article>
+                
+            </div>
+            <div class='flex-column'>
+               <img src='" . $data->Picture . "' class = '" . $data->class . "'>
+               
+            </div>
             ";
         }
         ?>
