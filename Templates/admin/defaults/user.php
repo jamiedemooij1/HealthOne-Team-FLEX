69a853748ed87f139d380db5668fac0070015387
Thierry Broersma <302677794@student.rocmondriaan.nl>
2021-12-02 10:01:38 +0100

    check

---
+++ Templates/admin/defaults/user.php
@@ -0,0 +1,22 @@
+<?php 
+
+    try {
+        
+        $db = new PDO("mysql:host=localhost;dbname=healthone", "root", "");
+        $query = $db->prepare("SELECT profile, info FROM customer WHERE username = :username");
+        $query->bindParam('username', $_SESSION['username']);
+        $query->execute();
+        $result = $query->fetchAll(PDO::FETCH_ASSOC);
+        foreach($result as &$data) {
+            echo "<h4>Account informatie</h4><br>";
+            echo "<img src='". $data['profile'] . "'";
+            echo "<br><br>";
+            echo "<h5>Persoonsinformatie</h5>";
+            echo "<p>" . $data['info'] . "</p>";
+        }
+          
+    } 
+    catch (PDOException $e) {
+        die("Error!: " . $e->getMessage());
+    }
+?>
