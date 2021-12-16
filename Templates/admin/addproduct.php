80ac3f83b9386462ba5c586635f7fb62a3ed32a3
Thierry Broersma <302677794@student.rocmondriaan.nl>
2021-12-16 10:31:22 +0100

    Het form van een apparaat toevoegen beter gestyled voor nu

--- Templates/admin/addproduct.php
+++ Templates/admin/addproduct.php
@@ -19,26 +19,17 @@ include_once('defaults/head.php');
   
     <form method="post" enctype="multipart/form-data">
     <tr>
-      <td><label for="" >ID </label>
-      <option value=""></option>
-      <select name="Category" id="">
-      <?php 
-      foreach ($categories as &$data) {
-          echo "<option value=" .  $data->Name . "></option> "; 
-      }
-
-      ?>
-      </select>
-      <input type="number" name="id" min=9 ></td>
-      <td><label for="" >Afbeelding </label>
-      <input type="file" name="img" ></td>
-      <td><label for="" >Naam </label>
-      <input type="text" name="name" ></td>
-      <td><label for="" >Beschrijving</label>
-      <input type="text" name="description" ></td>
-      <td><label for="" >Categorie ID</label>
-      <input type="number" min=1 max=4 name="category" >
-      <td><a href="/admin/productview/add"><input type="submit" value="Toevoegen" name="adding"></a></td>
+      <td><label for=""  class="addLabel">ID </label>
+      <input type="number" name="id" min=9 class="addForm"></td><br><br>
+      <td><label for="" class="addLabel"> Afbeelding </label>
+      <input type="file" name="img" class="addForm"></td><br><br>
+      <td><label for="" class="addLabel"> Naam </label>
+      <input type="text" name="name" class="addForm"></td><br><br>
+      <td><label for="" class="addLabel"> Beschrijving</label>
+      <input type="text" name="description" class="addForm"></td><br><br>
+      <td><label for="" class="addLabel"> Categorie ID</label>
+      <input type="number" min=1 max=4 name="category" class="addForm" ><br><br>
+      <td><a href="/admin/productview/add"><input type="submit" value="Toevoegen" name="adding" class="addForm"></a></td>
     </tr>
     </form>
 
